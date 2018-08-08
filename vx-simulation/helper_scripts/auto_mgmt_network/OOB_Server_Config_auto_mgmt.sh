80687a7a214e8a19fb87c924c6c7a52c786c06a1
Mike Courtney <michael.courtney@linkedup.net>
2018-08-07 20:32:35 -0500

    1533691955

--- vx-simulation/helper_scripts/auto_mgmt_network/OOB_Server_Config_auto_mgmt.sh
+++ vx-simulation/helper_scripts/auto_mgmt_network/OOB_Server_Config_auto_mgmt.sh
@@ -21,7 +21,7 @@ REPONAME="begin-ansible-training-bgp-nclu"
 #Install Automation Tools
 puppet=0
 ansible=1
-ansible_version=2.3.1.0
+ansible_version=2.6.2
 
 #######################
 
@@ -44,11 +44,11 @@ install_puppet(){
 }
 
 install_ansible(){
-    echo " ### Installing Ansible... ###"
-    apt-get install -qy ansible sshpass libssh-dev python-dev libssl-dev libffi-dev
-    pip install pip --upgrade
-    pip install setuptools --upgrade
-    pip install ansible==$ansible_version --upgrade
+	echo " ### Installing Ansible... ###"
+	apt-get install -qy ansible sshpass libssh-dev python-dev libssl-dev libffi-dev
+	sudo pip install pip --upgrade
+	sudo pip install setuptools --upgrade
+	sudo pip install ansible==$ansible_version --upgrade
 }
 
 ## MOTD
@@ -209,6 +209,10 @@ cat <<EOT >> /home/cumulus/.gitconfig
 [core]
     editor = vim
 EOT
+
+echo " ### Adding .bash_profile to auto login as cumulus user"
+echo "sudo su - cumulus" >> /home/vagrant/.bash_profile
+echo "exit" >> /home/vagrant/.bash_profile
 
 echo "############################################"
 echo "      DONE!"
